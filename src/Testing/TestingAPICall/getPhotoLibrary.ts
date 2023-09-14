12e45f4af40c20ce7f20ef3357ffe8ec6b9f87ad
sbrandon0432 <sbrandon0432@yahoo.com>
2023-09-14 15:42:38 -0400

    all dummy images added " ;

--- src/Testing/TestingAPICall/getPhotoLibrary.ts
+++ src/Testing/TestingAPICall/getPhotoLibrary.ts
@@ -2,6 +2,15 @@ import { PictureProps } from "../../Components/Context/types";
 import lion from "../TestingPhotoSet/Lion.png";
 import lionF from "../TestingPhotoSet/LionFull.png";
 import animeMan from "../TestingPhotoSet/animeMan.png";
+import girl from "../TestingPhotoSet/girl.png";
+import girl2 from "../TestingPhotoSet/girl2.png";
+import knight from "../TestingPhotoSet/knightLogo.png";
+import car from "../TestingPhotoSet/mustang.png";
+import superman2 from "../TestingPhotoSet/smanfull.png";
+import superman1 from "../TestingPhotoSet/supermanPort.png";
+import logo from "../TestingPhotoSet/swordLogo.png";
+import town from "../TestingPhotoSet/town.png";
+import tree from "../TestingPhotoSet/tree.png";
 
 // picture: string;
 // upVotes: number;
@@ -34,6 +43,78 @@ export const getPhotoLibrary = () => {
             downVotes: 1,
             id: 2,
             name: "Veteran adventurer",
+            isFavorite: false,
+        },
+        {
+            picture: girl,
+            upVotes: 12,
+            downVotes: 1,
+            id: 3,
+            name: "Anime Girl",
+            isFavorite: true,
+        },
+        {
+            picture: girl2,
+            upVotes: 12,
+            downVotes: 1,
+            id: 4,
+            name: "Anime Girl blond",
+            isFavorite: true,
+        },
+        {
+            picture: superman1,
+            upVotes: 12,
+            downVotes: 1,
+            id: 5,
+            name: "Super Man",
+            isFavorite: false,
+        },
+        {
+            picture: superman2,
+            upVotes: 12,
+            downVotes: 1,
+            id: 6,
+            name: "Super Man portrait",
+            isFavorite: false,
+        },
+        {
+            picture: car,
+            upVotes: 12,
+            downVotes: 1,
+            id: 7,
+            name: "car",
+            isFavorite: false,
+        },
+        {
+            picture: tree,
+            upVotes: 12,
+            downVotes: 1,
+            id: 8,
+            name: "tree",
+            isFavorite: true,
+        },
+        {
+            picture: town,
+            upVotes: 12,
+            downVotes: 1,
+            id: 9,
+            name: "town",
+            isFavorite: false,
+        },
+        {
+            picture: knight,
+            upVotes: 12,
+            downVotes: 1,
+            id: 10,
+            name: "Knight Logo",
+            isFavorite: false,
+        },
+        {
+            picture: logo,
+            upVotes: 12,
+            downVotes: 1,
+            id: 11,
+            name: "Logo",
             isFavorite: false,
         },
     ];
