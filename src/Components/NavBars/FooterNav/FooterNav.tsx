3087709348a95178db1f36a81a80773d7bf8d144
sbrandon0432 <sbrandon0432@yahoo.com>
2023-09-15 13:13:21 -0400

    footer svgs

--- src/Components/NavBars/FooterNav/FooterNav.tsx
+++ src/Components/NavBars/FooterNav/FooterNav.tsx
@@ -5,6 +5,7 @@ import home from "../../../Assets/Svgs/003-home3.svg";
 import userIcon from "../../../Assets/Svgs/114-user.svg";
 import heart from "../../../Assets/Svgs/219-heart.svg";
 import plus from "../../../Assets/Svgs/267-plus.svg";
+import film from "../../../Assets/Svgs/film.svg";
 import { Paths } from "../../../Types/routerTypes";
 
 import "./FooterNavS.scss";
@@ -21,22 +22,27 @@ export const FooterNav = () => {
             >
                 <Nav.Item className="navItem">
                     <Nav.Link eventKey={Paths.HOME}>
-                        <ReactSVG src={home} />
+                        <ReactSVG src={home} className="svg" />
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item className="navItem">
                     <Nav.Link eventKey={Paths.ADDPICTUREPAGE}>
-                        <ReactSVG src={plus} />
+                        <ReactSVG src={plus} className="svg" />
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="navItem">
+                    <Nav.Link eventKey={"/"}>
+                        <ReactSVG src={film} className="svg" />
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item className="navItem">
                     <Nav.Link eventKey="link-2">
-                        <ReactSVG src={heart} />
+                        <ReactSVG src={heart} className="svg" />
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item className="navItem">
                     <Nav.Link eventKey={Paths.PROFILE}>
-                        <ReactSVG src={userIcon} />
+                        <ReactSVG src={userIcon} className="svg" />
                     </Nav.Link>
                 </Nav.Item>
             </Nav>
