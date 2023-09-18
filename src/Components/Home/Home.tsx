522f0ced9836020133680fc214015887fbd9980d
sbrandon0432 <sbrandon0432@yahoo.com>
2023-09-18 13:47:05 -0400

    issue fixed
    ;

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,19 +1,11 @@
-import { useContext, useEffect, useState } from "react";
-import { getPhotoLibrary } from "../../Testing/TestingAPICall/getPhotoLibrary";
+import { useContext, useState } from "react";
 import { MyPictureContext } from "../Context/MyPicturesContext";
 import { RenderPictures } from "../RenderPictures/RenderPictures";
 import "./HomeS.scss";
 
 export const Home = () => {
-    const dummyLib = getPhotoLibrary();
     const { pictures, setPictures } = useContext(MyPictureContext);
     const [currentPictures, setCurrentPictures] = useState(pictures);
-
-    if (pictures.length < 2) setPictures(dummyLib);
-
-    useEffect(() => {
-        setCurrentPictures(pictures);
-    }, [pictures]);
 
     return (
         <div className="Home">
